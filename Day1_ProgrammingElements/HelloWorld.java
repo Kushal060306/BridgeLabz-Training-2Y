7709a8412ec428ea0b1a79766c4a53af2ccd227d
Ghost <komalpankaj81@gmail.com>
2025-08-17 23:18:15 +0530

    Initial commit for java fundamentals

--- Day1_ProgrammingElements/HelloWorld.java
+++ Day1_ProgrammingElements/HelloWorld.java
@@ -2,6 +2,10 @@ public class HelloWorld
 {
     public static void main(String[] args)
     {
+<<<<<<< Updated upstream
         System.out.println("Hello World Change");
+=======
+        System.out.println("Hello World Change in local");
+>>>>>>> Stashed changes
     }
 }
