5b2a17d7a8603bd1548a72abd1af7aedbce4548e
Kushal <kushalgupta06032006@gmail.com>
2025-09-21 18:22:11 +0530

    Delete 02 Java Programming Elements Level 1 Lab Practice directory

--- 02 Java Programming Elements Level 1 Lab Practice/harrys age.java
+++
@@ -1,9 +0,0 @@
-public class Harrys_Age {
-    public static void main(String[] args) {
-         int Birth_year=2000;
-         int Current_year=2024;
-          int present_age = Current_year-Birth_year;
-
-          System.out.println("Harry's age in "+Current_year+" is "+ present_age);
-    }
-}
